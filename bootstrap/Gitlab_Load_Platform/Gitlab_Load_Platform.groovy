3cc4e60ea6886b1067ef3d6ab072b2455cbcc1bf
Teodoro, John Arcy R <john.arcy.r.teodoro@accenture.com>
2016-04-20 23:00:42 +0800

    added PASSWORD_GITLAB variable to shell scripts

--- bootstrap/Gitlab_Load_Platform/Gitlab_Load_Platform.groovy
+++ bootstrap/Gitlab_Load_Platform/Gitlab_Load_Platform.groovy
@@ -18,26 +18,27 @@ generateGitlabLoadPlatformJob.with {
 		}
 		steps {
 				shell('''#!/bin/bash -ex
-						echo -e "Host gitlab\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-						echo -e "Host innersource.accenture.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+PASSWORD_GITLAB=${PASSWORD_GITLAB:-gitlab1234}
+				
+echo -e "Host gitlab\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host innersource.accenture.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 						
-						token="$(curl -X POST "http://gitlab:9080/api/v3/session?login=root&password=gitlab1234" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['private_token'];")"
+token="$(curl -X POST "http://gitlab:9080/api/v3/session?login=root&password=${PASSWORD_GITLAB}" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['private_token'];")"
 						
-						key=$(cat ~/.ssh/id_rsa.pub)
+key=$(cat ~/.ssh/id_rsa.pub)
 
-						curl --header "PRIVATE-TOKEN: $token" -X POST "http://gitlab:9080/api/v3/user/keys" --data-urlencode "title=jenkins@adop-core" --data-urlencode "key=${key}"
+curl --header "PRIVATE-TOKEN: $token" -X POST "http://gitlab:9080/api/v3/user/keys" --data-urlencode "title=jenkins@adop-core" --data-urlencode "key=${key}"
 
-						# create platform-management into gitlab
-						target_repo_name="platform-management"
+# create platform-management into gitlab
+target_repo_name="platform-management"
 
-						curl --header "PRIVATE-TOKEN: $token" -X POST "http://gitlab:9080/api/v3/projects?name=${target_repo_name}"
+curl --header "PRIVATE-TOKEN: $token" -X POST "http://gitlab:9080/api/v3/projects?name=${target_repo_name}"
 
-						# Create Gerrit repository
-						# push the sample codes to the sample Gitlab project
-						git remote add adop git@gitlab:root/$target_repo_name.git
-						git fetch adop
-						git push adop +refs/remotes/origin/*:refs/heads/*
-				''')
+# Create Gerrit repository
+# push the sample codes to the sample Gitlab project
+git remote add adop git@gitlab:root/$target_repo_name.git
+git fetch adop
+git push adop +refs/remotes/origin/*:refs/heads/*''')
 			dsl{
 						external('bootstrap/**/*.groovy')
 						ignoreExisting()
